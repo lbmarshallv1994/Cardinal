21a021545c877314dbdbf8f74d35538a247edc57
Benjamin Murphy <benjamin.addington.murphy@gmail.com>
2020-01-31 15:14:12 -0500

    created prox calc

---
+++ Open-ILS/src/perlmods/lib/OpenILS/Utils/ProximityCalculator.pm
@@ -0,0 +1,11 @@
+package OpenILS::Utils::ProximityCalculator;
+use WWW::REST;
+use OpenILS::Application::Actor;
+
+sub proximity {
+    my $org1 = shift;
+    my $org2 = shift;
+    my $org1addr = OpenILS::Application::Actor::retrieve_org_address($org1);
+    my $org2addr = OpenILS::Application::Actor::retrieve_org_address($org2);
+    print($org1addr . '   ' . $org2addr);
+}
