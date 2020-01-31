ebb2ddcf3b2d80d65546435f7212683fbd900d9a
Benjamin Murphy <benjamin.addington.murphy@gmail.com>
2020-01-31 16:18:45 -0500

    proxcalc update

--- Open-ILS/src/perlmods/lib/OpenILS/Utils/ProximityCalculator.pm
+++ Open-ILS/src/perlmods/lib/OpenILS/Utils/ProximityCalculator.pm
@@ -3,10 +3,33 @@ use strict; use warnings;
 use WWW::REST;
 use OpenILS::Application::Actor;
 
+sub get_ou {
+    my $org = shift || '-';
+    my $org_unit;
+
+    if ($org eq '-') {
+         $org_unit = $actor->request(
+            'open-ils.actor.org_unit_list.search' => parent_ou => undef
+        )->gather(1);
+    } elsif ($org !~ /^\d+$/o) {
+         $org_unit = $actor->request(
+            'open-ils.actor.org_unit_list.search' => shortname => uc($org)
+        )->gather(1);
+    } else {
+         $org_unit = $actor->request(
+            'open-ils.actor.org_unit_list.search' => id => $org
+        )->gather(1);
+    }
+
+    return $org_unit;
+}
+
 sub proximity {
-    my( $self, $org1, $org2 ) = @_;
-    my $org1addr = OpenILS::Application::Actor::retrieve_org_address($org1);
-    my $org2addr = OpenILS::Application::Actor::retrieve_org_address($org2);
+    my( $self, $org1id, $org2id ) = @_;
+    my $org1 = get_ou($org1id)->[0];
+    my $org2 = get_ou($org2id)->[0];
+    my $org1addr = OpenILS::Application::Actor::retrieve_org_address($org1->mailing_address);
+    my $org2addr = OpenILS::Application::Actor::retrieve_org_address($org2->mailing_address);
     print($org1 . ' ' . $org2);
     print($org1addr . '   ' . $org2addr);
 }
