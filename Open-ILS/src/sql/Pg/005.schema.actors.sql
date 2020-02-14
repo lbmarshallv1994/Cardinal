b0c02813afb5ccd9c2f100567c5fe7b6cf179732
Llewellyn Marshall <llewellyn.marshall@ncdcr.gov>
2020-02-14 15:32:02 -0500

    update schema pt 2

--- Open-ILS/src/sql/Pg/005.schema.actors.sql
+++ Open-ILS/src/sql/Pg/005.schema.actors.sql
@@ -1276,4 +1276,65 @@ CREATE TABLE actor.usr_privacy_waiver (
 );
 CREATE INDEX actor_usr_privacy_waiver_usr_idx ON actor.usr_privacy_waiver (usr);
 
+CREATE TABLE actor.org_unit_shipping_hub (
+    id SERIAL PRIMARY KEY,
+    org_unit BIGINT NOT NULL REFERENCES actor.org_unit(id) ON DELETE CASCADE DEFERRABLE,
+    hub BIGINT NOT NULL REFERENCES actor.org_unit(id) ON DELETE CASCADE DEFERRABLE
+);
+
+CREATE OR REPLACE FUNCTION actor.org_unit_ancestor_shipping_hub(org_id integer)
+  RETURNS SETOF actor.org_unit_shipping_hub AS
+$func$
+DECLARE
+    shipping_hub record;
+    cur_org INT;
+BEGIN
+    cur_org := org_id;
+    LOOP
+        SELECT INTO shipping_hub * FROM actor.org_unit_shipping_hub WHERE org_unit = cur_org;
+        IF FOUND THEN
+            RETURN NEXT shipping_hub;
+            EXIT;
+        END IF;
+        SELECT INTO cur_org parent_ou FROM actor.org_unit WHERE id = cur_org;
+        EXIT WHEN cur_org IS NULL;
+    END LOOP;
+    RETURN;
+END;
+$func$ LANGUAGE PLPGSQL;
+
+
+CREATE OR REPLACE FUNCTION actor.list_org_unit_ancestor_shipping_hub(VARIADIC orgs NUMERIC[]) RETURNS TABLE(org_unit INT,hub INT)
+  AS
+$func$
+DECLARE
+    rec record;
+    cur_org INT;
+    next_hub INT;
+    org_id INT;
+BEGIN
+    FOREACH org_id IN ARRAY orgs LOOP
+    cur_org := org_id;
+    org_unit := cur_org;
+    LOOP
+        SELECT INTO next_hub actor.org_unit_shipping_hub.hub FROM actor.org_unit_shipping_hub WHERE actor.org_unit_shipping_hub.org_unit = cur_org;
+        IF FOUND THEN
+            hub := next_hub;
+            return next;
+            EXIT;
+        END IF;
+        SELECT INTO cur_org parent_ou FROM actor.org_unit WHERE actor.org_unit.id = cur_org;
+        EXIT WHEN cur_org IS NULL;
+    END LOOP;
+    END LOOP;
+    RETURN;
+END;
+$func$ LANGUAGE PLPGSQL;
+
+CREATE TABLE actor.org_unit_shipping_hub_distance (
+    id SERIAL PRIMARY KEY,
+    orig_hub BIGINT NOT NULL REFERENCES actor.org_unit(id) ON DELETE CASCADE DEFERRABLE,
+    dest_hub BIGINT NOT NULL REFERENCES actor.org_unit(id) ON DELETE CASCADE DEFERRABLE,
+    distance INT NOT NULL
+);
 COMMIT;
