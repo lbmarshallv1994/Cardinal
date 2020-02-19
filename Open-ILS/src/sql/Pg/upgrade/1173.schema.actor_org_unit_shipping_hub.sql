0964aa7f1ce50a14c02bf573475eb9ecda3dc3b6
Llewellyn Marshall <llewellyn.marshall@ncdcr.gov>
2020-02-19 13:54:53 -0500

    remove unused function

--- Open-ILS/src/sql/Pg/upgrade/1173.schema.actor_org_unit_shipping_hub.sql
+++ Open-ILS/src/sql/Pg/upgrade/1173.schema.actor_org_unit_shipping_hub.sql
@@ -5,28 +5,6 @@ INSERT INTO config.upgrade_log (version) VALUES ('1173');
 ALTER TABLE actor.org_unit
 ADD COLUMN shipping_hub_ou BIGINT REFERENCES actor.org_unit(id) ON DELETE SET NULL;
 
-CREATE OR REPLACE FUNCTION actor.org_unit_ancestor_shipping_hub(org_id integer)
-  RETURNS SETOF actor.org_unit_shipping_hub AS
-$func$
-DECLARE
-    shipping_hub record;
-    cur_org INT;
-BEGIN
-    cur_org := org_id;
-    LOOP
-        SELECT INTO shipping_hub actor.org_unit.shipping_hub_ou FROM actor.org_unit WHERE id = cur_org;
-        IF FOUND THEN
-            RETURN NEXT shipping_hub;
-            EXIT;
-        END IF;
-        SELECT INTO cur_org parent_ou FROM actor.org_unit WHERE id = cur_org;
-        EXIT WHEN cur_org IS NULL;
-    END LOOP;
-    RETURN;
-END;
-$func$ LANGUAGE PLPGSQL;
-
-
 CREATE OR REPLACE FUNCTION actor.list_org_unit_ancestor_shipping_hub(VARIADIC orgs NUMERIC[]) RETURNS TABLE(org_unit INT,hub INT)
   AS
 $func$
