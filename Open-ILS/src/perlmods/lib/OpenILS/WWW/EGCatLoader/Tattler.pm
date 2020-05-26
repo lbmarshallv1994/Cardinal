6b9c185053998a679601535b83a8a4bd4792f9e0
Llewellyn Marshall <llewellyn.marshall@ncdcr.gov>
2020-05-26 13:15:55 -0400

    test having a filler db update and template

--- Open-ILS/src/perlmods/lib/OpenILS/WWW/EGCatLoader/Tattler.pm
+++ Open-ILS/src/perlmods/lib/OpenILS/WWW/EGCatLoader/Tattler.pm
@@ -13,15 +13,19 @@ sub update_tattle_list {
     my %kwargs = @_;
     my $ctx = $self->ctx;
     my $cgi = $self->cgi;
-    foreach ($cgi->param) {
-        my $val = $cgi->param($_);
-        $self->inspect_register_value($_, $val);
-        $logger->debug("$_ $val");
-        
-    }
+
+    my $dist = Fieldmapper::config::tattler_ignore_list->new;
+    $dist->org_unit(1);
+    $dist->target_copy(2);
+    $dist->report_name("test");
+    $self->{editor}->runmethod('create', 'config.tattler_ignore_list', 'igl', $dist);
     return Apache2::Const::HTTP_BAD_REQUEST
             unless $cgi->request_method eq 'POST';
-
+    foreach ($cgi->param) {
+        my $val = $cgi->param($_);
+        #$self->inspect_register_value($_, $val);
+        $logger->debug("$_ $val");       
+    }
 }
 
 1;
