4ce1bd3aac41d0d62598d9f7db8b1187dfb0bc14
Bill Erickson <berickxx@gmail.com>
2020-06-24 12:25:16 -0400

    LP1884787 ng lint and unit test repairs
    Minor lint update, some of which affected tests and required a few
    additional test spec updates.
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- Open-ILS/src/eg2/src/app/staff/booking/reservation-actions.spec.ts
+++ Open-ILS/src/eg2/src/app/staff/booking/reservation-actions.spec.ts
@@ -1,30 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie';
-import { PrintService } from '@eg/share/print/print.service';
 import { PcrudService } from '@eg/core/pcrud.service';
 import { ReservationActionsService } from './reservation-actions.service';
 describe('ReservationActionsService', () => {
     let service: ReservationActionsService;
-    let cookieServiceStub: Partial<CookieService>;
-    let printServiceStub: Partial<PrintService>;
     let pcrudServiceStub: Partial<PcrudService>;
     const routerSpy = {
         navigate: jasmine.createSpy('navigate')
     };
     beforeEach(() => {
         pcrudServiceStub = {};
-        cookieServiceStub = {};
-        pcrudServiceStub = {};
-        cookieServiceStub = {};
-        printServiceStub = {};
         TestBed.configureTestingModule({
             providers: [
                 ReservationActionsService,
                 { provide: Router, useValue: routerSpy },
-                { provide: PcrudService, useValue: pcrudServiceStub },
-                { provide: CookieService, useValue: cookieServiceStub },
-                { provide: PrintService, useValue: printServiceStub }
+                { provide: PcrudService, useValue: pcrudServiceStub }
             ]
         });
         service = TestBed.get(ReservationActionsService);
