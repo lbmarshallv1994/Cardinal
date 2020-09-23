a6311fafbd248f7d4e419dc7802423ef3ae15f34
Chris Sharp <csharp@georgialibraries.org>
2020-09-23 16:33:06 -0400

    LP#1835127 - Stamp upgrade script
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>

--- Open-ILS/src/sql/Pg/upgrade/1238.data.permission.booking_reservation.sql
+++ Open-ILS/src/sql/Pg/upgrade/1238.data.permission.booking_reservation.sql
@@ -1,11 +1,11 @@
 BEGIN;
 
-SELECT evergreen.upgrade_deps_block_check('1238', :eg_version);
+SELECT evergreen.upgrade_deps_block_check('XXXX', :eg_version);
 
 INSERT INTO permission.perm_list ( id, code, description ) VALUES
- ( 625, 'VIEW_BOOKING_RESERVATION', oils_i18n_gettext(625,
+ ( 623, 'VIEW_BOOKING_RESERVATION', oils_i18n_gettext(623,
     'View booking reservations', 'ppl', 'description')),
- ( 626, 'VIEW_BOOKING_RESERVATION_ATTR_MAP', oils_i18n_gettext(626,
+ ( 624, 'VIEW_BOOKING_RESERVATION_ATTR_MAP', oils_i18n_gettext(624,
     'View booking reservation attribute maps', 'ppl', 'description'))
 ;
 
