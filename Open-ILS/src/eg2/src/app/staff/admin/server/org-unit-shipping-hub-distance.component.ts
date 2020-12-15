1e628ca0de3b4749f750292eb857c620a427f0c3
Llewellyn Marshall <llewellyn.marshall@ncdcr.gov>
2020-12-15 14:53:02 -0500

    reeeeeeeeevert

--- Open-ILS/src/eg2/src/app/staff/admin/server/org-unit-shipping-hub-distance.component.ts
+++ Open-ILS/src/eg2/src/app/staff/admin/server/org-unit-shipping-hub-distance.component.ts
@@ -16,9 +16,9 @@ import {NetService} from '@eg/core/net.service';
       <div class="alert alert-info" i18n>
         Entries in this table contain the distance in miles between shipping locations. Shipping hubs are configured from the Orginizational Units page. These numbers are used for sorting hold targets during inter-library lending. Entries can be created manually or calculated using the free Bing Maps API if a key has been set up in the opensrf core config file. <b>Running the API will remove any existing data from this table</b>.    
       </div>
-      <div ngIf="!calculating"> 
+      
         <button class="btn btn-outline-dark" (click)="calculateDistances()">Calculate with API</button>
-      </div>
+ 
       <br>
       <br>
       <eg-admin-page persistKeyPfx="{{persistKeyPfx}}" idlClass="{{idlClass}}"
